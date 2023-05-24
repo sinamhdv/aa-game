16497f90f5cc1671d5cc24d5af8579fb7e06776c
sinamhdv <sinamhdv@gmail.com>
2023-05-24 16:50:58 +0330

    added main menu buttons

--- src/main/java/aa/view/MainMenu.java
+++ src/main/java/aa/view/MainMenu.java
@@ -1,12 +1,38 @@
 package aa.view;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainMenu extends Application {
+	public void newGameButtonClicked(MouseEvent mouseEvent) {
+		System.out.println("new game");
+	}
+
+	public void continueButtonClicked(MouseEvent mouseEvent) {
+		System.out.println("continue");
+	}
+
+	public void profileButtonClicked(MouseEvent mouseEvent) {
+		System.out.println("profile");
+	}
+
+	public void scoreboardButtonClicked(MouseEvent mouseEvent) {
+		System.out.println("scoreboard");
+	}
+
+	public void optionsButtonClicked(MouseEvent mouseEvent) {
+		System.out.println("options");
+	}
+
+	public void exitButtonClicked(MouseEvent mouseEvent) {
+		Platform.exit();
+	}
+
 	@Override
 	public void start(Stage stage) throws Exception {
 		BorderPane borderPane = FXMLLoader.load(MainMenu.class.getResource("/fxml/MainMenu.fxml"));
