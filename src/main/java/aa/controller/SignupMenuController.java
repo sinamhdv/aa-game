9b07664a069132bdbe1ee97846b1cbb5acf1babf
sinamhdv <sinamhdv@gmail.com>
2023-05-24 15:28:58 +0330

    added random avatar and manual avatar selection

--- src/main/java/aa/controller/SignupMenuController.java
+++ src/main/java/aa/controller/SignupMenuController.java
@@ -2,16 +2,24 @@ package aa.controller;
 
 import aa.controller.messages.SignupMenuMessage;
 import aa.model.Globals;
-import aa.model.User;
 import aa.utils.FormatValidation;
 
 public class SignupMenuController {
+	private static String avatarFilePath;
+	public static String getAvatarFilePath() {
+		return avatarFilePath;
+	}
+	public static void setAvatarFilePath(String avatarFilePath) {
+		SignupMenuController.avatarFilePath = avatarFilePath;
+	}
+
 	public static SignupMenuMessage signup(String username, String password) {
 		if (Globals.getUserByName(username) != null)
 			return SignupMenuMessage.USER_ALREADY_EXISTS;
 		if (!FormatValidation.checkUsernameFormat(username))
 			return SignupMenuMessage.INVALID_USERNAME_FORMAT;
-		new User(username, password);
+		System.out.println(avatarFilePath);
+		// new User(username, password);
 		return SignupMenuMessage.SUCCESS;
 	}
 }
--- src/main/java/aa/view/eventcontrollers/SignupMenuEventController.java
+++ src/main/java/aa/view/eventcontrollers/SignupMenuEventController.java
@@ -1,5 +1,8 @@
 package aa.view.eventcontrollers;
 
+import java.io.File;
+import java.util.Random;
+
 import aa.controller.SignupMenuController;
 import aa.controller.messages.SignupMenuMessage;
 import aa.view.LoginMenu;
@@ -13,6 +16,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 
 public class SignupMenuEventController {
 	@FXML
@@ -23,9 +27,12 @@ public class SignupMenuEventController {
 	private Label errorText;
 	@FXML
 	private HBox avatarsBox;
+	private String[] avatarImagesPath = new String[6];
 	private RadioButton[] avatarRadioButtons = new RadioButton[6];
 
 	public void signupButtonClicked(MouseEvent mouseEvent) throws Exception {
+		if (avatarRadioButtons[4].isSelected())
+			pickRandomAvatar();
 		SignupMenuMessage message = SignupMenuController.signup(
 			usernameTextField.getText(),
 			passwordField.getText()
@@ -42,25 +49,47 @@ public class SignupMenuEventController {
 		new LoginMenu().start(LoginMenu.getStage());
 	}
 
+	public void chooseAvatarFile(MouseEvent mouseEvent) {
+		FileChooser fileChooser = new FileChooser();
+		File file = fileChooser.showOpenDialog(LoginMenu.getStage());
+		if (file != null)
+			SignupMenuController.setAvatarFilePath("file:" + file.getAbsolutePath());
+	}
+
+	public void pickDefaultAvatar(int index) {
+		SignupMenuController.setAvatarFilePath(avatarImagesPath[index]);
+	}
+
+	public void pickRandomAvatar() {
+		pickDefaultAvatar(new Random().nextInt(4));
+	}
+
 	@FXML
 	private void initialize() {
+		SignupMenuController.setAvatarFilePath(null);
+
+		for (int i = 0; i < 4; i++)
+			avatarImagesPath[i] = SignupMenuEventController.class.getResource("/images/avatars/" + i + ".jpg").toExternalForm();
+		for (int i = 4; i < 6; i++)
+			avatarImagesPath[i] = SignupMenuEventController.class.getResource("/images/avatars/" + i + ".png").toExternalForm();
+
 		ToggleGroup toggleGroup = new ToggleGroup();
 		for (int i = 0; i < avatarRadioButtons.length; i++) {
 			RadioButton radio = new RadioButton();
 			radio.setToggleGroup(toggleGroup);
 			ImageView image;
-			try {
-				image = new ImageView(
-					SignupMenuEventController.class.getResource("/images/avatars/" + i + ".jpg").toExternalForm());
-			} catch (NullPointerException ex) {
-				image = new ImageView(
-					SignupMenuEventController.class.getResource("/images/avatars/" + i + ".png").toExternalForm());
-			}
+			image = new ImageView(avatarImagesPath[i]);
 			image.setFitHeight(50);
 			image.setFitWidth(50);
 			radio.setGraphic(image);
 			avatarRadioButtons[i] = radio;
 			avatarsBox.getChildren().add(radio);
 		}
+		for (int i = 0; i < 4; i++) {
+			int index = i;
+			avatarRadioButtons[i].setOnMouseClicked(event -> { pickDefaultAvatar(index); });
+		}
+		avatarRadioButtons[4].setSelected(true);
+		avatarRadioButtons[5].setOnMouseClicked(this::chooseAvatarFile);
 	}
 }
